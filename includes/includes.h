3345d8b461e0fa453588e2fdf383468c9cecabb3
k3rb3ros <k3rb3ros@k3rb3ros.com>
2013-03-20 23:39:42 -0600

    Added seperate windows and linux SDL header files to fix windows compile error

--- includes/includes.h
+++ includes/includes.h
@@ -1,8 +1,16 @@
 #ifndef INCLUDES_H
 #define INCLUDES_H
 
-	#include "SDL.h"
-	#include "SDL_ttf.h"
+	#ifdef __linux__
+		#include "../includes/linux/SDL.h"
+		#include "../includes/linux/SDL_ttf.h"
+	#endif
+
+	#ifdef _WIN32
+		#include "../includes/windows/SDL.h"
+		#include "../includes/windows/SDL_ttf.h"
+	#endif
+
 	#include <iostream>
 	#include <sstream>
 	#include <string>
