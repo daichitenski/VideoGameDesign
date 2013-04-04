3adde304eb0c66ac8accc19ddb5a251999b750d8
k3rb3ros <k3rb3ros@k3rb3ros.com>
2013-04-03 18:58:58 -0600

    Added header file for cards starting to divide cards.cpp into respective classes, also some of those classes can be unified to more general classes

---
+++ includes/cards.h
@@ -0,0 +1,24 @@
+#ifndef CARD_H
+#define CARD_H
+
+	#include "../includes/includes.h"
+	
+	const int SCREENWIDTH = 800;
+	const int SCREENHEIGHT= 600;
+	const int CARDHEIGHT = 120;
+	const int CARDWIDTH = 90;
+	const int CARDWIDTH_WITH_OFFSET = 110;
+	const int CARDHEIGHT_SMALL = 48;
+	const int CARDWIDTH_SMALL = 37;
+	string SPRITESHEET = "../images/cardfaces.bmp";
+	string BG_IMAGE = "../images/background.bmp";
+	string SPRITESHEET_S = "../images/cardfaces_s.bmp";
+	const int CARDSHEET_WIDTH = 564;
+
+	//Card classes will go here
+	/*
+	class
+	{
+		
+	};*/
+#endif
--- source/cards.cpp
+++ source/cards.cpp
@@ -1,17 +1,4 @@
-#include "../includes/includes.h"
-
-const int SCREENWIDTH = 800;
-const int SCREENHEIGHT= 600;
-const int CARDHEIGHT = 120;
-const int CARDWIDTH = 90;
-const int CARDWIDTH_WITH_OFFSET = 110;
-const int CARDHEIGHT_SMALL = 48;
-const int CARDWIDTH_SMALL = 37;
-string SPRITESHEET = "../images/cardfaces.bmp";
-string BG_IMAGE = "../images/background.bmp";
-string SPRITESHEET_S = "../images/cardfaces_s.bmp";
-const int CARDSHEET_WIDTH = 564;
-
+#include "../includes/cards.h"
 
 //Image Classes
 class CardImage{
